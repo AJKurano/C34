28d5ef77f891b58fef425b1f24678284f517d6d0
Anay Jaiswal <anayjaiswal2905@gmail.com>
2020-09-23 18:03:16 +0530

    Multiplayer Car racing game with fire base configuration

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 var ball;
+var database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPosition = database.ref("Ball/Position");
+    ballPosition.on("value", readPosition, showErr);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +31,22 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function writePosition(x, y){
+    console.log(position);
+    database.ref("Ball/Position").set({
+        'X' : position.X + x,
+        'Y' : position.Y + y
+    })
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.X;
+    ball.y = position.Y;
+}
+
+function showErr(){
+    console.log("Error occured");
 }
